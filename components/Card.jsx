d0938719094911920baf020fdc902a44d0634e1a
mqkhan <qasim_ajk@yahoo.com>
2024-01-11 09:59:59 +0100

    first commit

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,8 @@
-export default function Card(href, imgAlt, imgSrc) {
+export default function Card({ href, imgAlt, imgSrc }) {
   return (
     <>
       <div className="card">
-        <img src={imgSrc} alt={imgAlt} />
+        <img src={imgSrc} alt={imgAlt} width={100} height={100} />
         <a href={href}>my pic</a>
       </div>
     </>
